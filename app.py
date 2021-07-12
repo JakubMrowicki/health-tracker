2de1190213e694045f38280ee4b7fafa79c475f5
JakubMrowicki <jakubmrowicki96@gmail.com>
2021-07-12 16:27:09 +0000

    add functionality to add new entries

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
+from datetime import datetime
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
@@ -18,6 +19,14 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 
 mongo = PyMongo(app)
+
+
+def isLogged():
+    try:
+        if session["user"]:
+            return True
+    except KeyError:
+        return False
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -37,25 +46,27 @@ def home():
         else:
             flash("Incorrect username or password. Please try again.")
             return redirect(url_for("home"))
-    try:
-        if session["user"]:
-            # if logged in, take to the dashboard
-            return feed()
-    except KeyError:
+    if not isLogged():
         return render_template("welcome.html")
+    else:
+        return feed()
 
 
 def feed():
     user_info = mongo.db.users.find_one({"username": session["user"]})
     entries = list(mongo.db.entries.find(
-        {"user": session["user"]}).sort("timestamp", -1))
+        {"user": session["user"]}))
     return render_template("feed.html", entries=entries, user=user_info)
 
 @app.route("/signout")
 def signout():
-    flash("You've been signed out.")
-    session.pop("user")
-    return redirect(url_for("home"))
+    if not isLogged():
+        flash("You must be logged in to access this page.")
+        return render_template("welcome.html")
+    else:
+        flash("You've been signed out.")
+        session.pop("user")
+        return redirect(url_for("home"))
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -85,7 +96,26 @@ def register():
 
 @app.route("/new", methods=["GET", "POST"])
 def new():
-    return render_template("new.html")
+    if not isLogged():
+        flash("You must be logged in to access this page.")
+        return render_template("welcome.html")
+    else:
+        if request.method == "POST":
+            current_time = datetime.now()
+            body = request.form.get("body") if len(request.form.get("body")) > 1 else None
+            pinned = True if request.form.get("pinned") == "pin" else False
+            entry = {
+                "title": request.form.get("title"),
+                "body": body,
+                "type": request.form.get("type"),
+                "pinned": pinned,
+                "date": current_time.strftime("%d/%m/%Y"),
+                "user": session["user"]
+            }
+            mongo.db.entries.insert_one(entry)
+            flash("Your entry has been added!")
+            return redirect(url_for("home"))
+        return render_template("new.html")
 
 
 if __name__ == "__main__":
